261418f94a45edfdeec9cedaa23d6fb6e03de161
Cheikh <bayecheikhgueye2@gmail.com>
2022-05-14 18:36:34 +0000

    Update

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 use Validator;
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin');
+    }
     /**
      * Display a listing of the resource.
      *
