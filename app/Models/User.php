39f168c507719d1e218ab0f60f6ff8fe6440ff8e
Cheikh <bayecheikhgueye2@gmail.com>
2022-06-12 01:32:13 +0000

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use App\Permissions\HasPermissionsTrait;
 class User extends Authenticatable
 {
     use Notifiable, HasApiTokens, HasPermissionsTrait;
+    protected $primaryKey   = 'id';
  
     /**
      * The attributes that are mass assignable.
