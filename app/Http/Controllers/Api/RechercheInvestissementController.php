07ade7d877a8a808b876c16f877b58aa6cb0f056
Cheikh <bayecheikhgueye2@gmail.com>
2022-06-14 22:45:06 +0000

    Update

--- app/Http/Controllers/Api/RechercheInvestissementController.php
+++ app/Http/Controllers/Api/RechercheInvestissementController.php
@@ -59,17 +59,58 @@ class RechercheInvestissementController extends Controller
             ->json($validator->errors());
         }
         else{ 
-            $investissements = Investissement::with('region')
-            ->with('annee')
-            ->with('monnaie')
-            ->with('structure')
-            ->with('source')
-            ->with('dimension')
-            ->with('mode_financements')
-            ->with('ligne_financements')
-            ->with('piliers')
-            ->with('axes')
-            ->with('fichiers');
+
+            if ($request->user()->hasRole('super_admin') || $request->user()->hasRole('admin_dprs')) {
+                $investissements = Investissement::with('region')
+                ->with('annee')
+                ->with('monnaie')
+                ->with('structure')
+                ->with('source')
+                ->with('dimension')
+                ->with('piliers')
+                ->with('axes')
+                ->with('mode_financements')
+                ->with('ligne_financements')
+                ->with('fichiers')
+                ->paginate(10);
+            }
+            else{
+                if($request->user()->hasRole('directeur_eps')){
+                    $source_id = User::find($request->user()->id)->structures[0]->source_financements[0]->id;
+                    $investissements = Investissement::with('region')
+                    ->with('annee')
+                    ->with('monnaie')
+                    ->with('structure')
+                    ->with('source')
+                    ->with('dimension')
+                    ->with('piliers')
+                    ->with('axes')
+                    ->with('mode_financements')
+                    ->with('ligne_financements')
+                    ->with('fichiers')
+                    ->whereHas('source', function($q) use ($source_id){
+                        $q->where('id', $source_id);
+                    });
+                }
+                else{
+                    $structure_id = User::find($request->user()->id)->structures[0]->id;
+                    $investissements = Investissement::with('region')
+                    ->with('annee')
+                    ->with('monnaie')
+                    ->with('structure')
+                    ->with('source')
+                    ->with('dimension')
+                    ->with('piliers')
+                    ->with('axes')
+                    ->with('mode_financements')
+                    ->with('ligne_financements')
+                    ->with('fichiers')
+                    ->whereHas('structure', function($q) use ($structure_id){
+                        $q->where('id', $structure_id);
+                    });
+                }
+                
+            }
 
             if($annee!=null){               
                 $investissements = $investissements->whereHas('annee', function($q) use ($annee){
