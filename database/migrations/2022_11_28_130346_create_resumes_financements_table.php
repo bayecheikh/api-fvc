46379ba7992d6199145fb3949cdecae226ba9848
Cheikh <bayecheikhgueye2@gmail.com>
2022-11-28 13:08:20 +0000

    Update

---
+++ database/migrations/2022_11_28_130346_create_resumes_financements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resumes_fines', function (Blueprint $table) {
+            $table->unsignedInteger('fichier_id');
+            $table->unsignedInteger('financement_id');
+            $table->primary(['fichier_id','financement_id'],'fichier_fine_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resumes_financements');
+    }
+};
