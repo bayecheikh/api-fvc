d3fd053023517df1af1f9f394077a3f38c768e74
bayecheikh <bayecheikhgueye2@gmail.com>
2025-12-15 13:45:44 +0000

    first commit

--- app/Models/Financement.php
+++ app/Models/Financement.php
@@ -29,50 +29,50 @@ class Financement extends Model
        'volume_co2',
        'status',
        'state',
-       'motif_rejet', 
+       'motif_rejet',
        'brouillon'
     ];
 
     public function annee() {
-        return $this->belongsToMany(Annee::class,'annees_fines');          
+        return $this->belongsToMany(Annee::class,'annees_fines');
     }
     public function domaine_financement() {
-        return $this->belongsToMany(DomaineFinancement::class,'domaine_fines_fines');          
+        return $this->belongsToMany(DomaineFinancement::class,'domaine_fines_fines');
     }
     public function source_financement() {
-        return $this->belongsToMany(SourceFinancement::class,'source_fines_fines');          
+        return $this->belongsToMany(SourceFinancement::class,'source_fines_fines');
     }
     public function objectif_adaptations() {
-        return $this->belongsToMany(ObjectifAdaptation::class,'objectif_adaptations_fines');          
+        return $this->belongsToMany(ObjectifAdaptation::class,'objectif_adaptations_fines');
     }
     public function objectif_attenuations() {
-        return $this->belongsToMany(ObjectifAttenuation::class,'objectif_attenuations_fines');          
+        return $this->belongsToMany(ObjectifAttenuation::class,'objectif_attenuations_fines');
     }
     public function objectif_transversals() {
-        return $this->belongsToMany(ObjectifTransversal::class,'objectif_transversals_fines');          
+        return $this->belongsToMany(ObjectifTransversal::class,'objectif_transversals_fines');
     }
     public function agence_acredite() {
-        return $this->belongsToMany(AgenceAcredite::class,'agence_acredites_fines');      
+        return $this->belongsToMany(AgenceAcredite::class,'agence_acredites_fines');
     }
     public function ligne_financement_bailleurs() {
-        return $this->belongsToMany(LigneFinancementBailleur::class,'ligne_fine_bailleurs_fines');          
+        return $this->belongsToMany(LigneFinancementBailleur::class,'ligne_fine_bailleurs_fines');
     }
     public function ligne_financement_cos() {
-        return $this->belongsToMany(LigneFinancementCo::class,'ligne_fine_cos_fines');          
+        return $this->belongsToMany(LigneFinancementCo::class,'ligne_fine_cos_fines');
     }
     public function ligne_financement_secteurs() {
-        return $this->belongsToMany(LigneFinancementSecteur::class,'ligne_fine_secteurs_fines');          
+        return $this->belongsToMany(LigneFinancementSecteur::class,'ligne_fine_secteurs_fines');
     }
     public function ligne_financement_zones() {
-        return $this->belongsToMany(LigneFinancementZone::class,'ligne_fine_zones_fines');          
+        return $this->belongsToMany(LigneFinancementZone::class,'ligne_fine_zones_fines');
     }
     public function structure() {
-        return $this->belongsToMany(Structure::class,'structures_fines');          
-    }   
+        return $this->belongsToMany(Structure::class,'structures_fines');
+    }
     public function resumes() {
-        return $this->belongsToMany(Fichier::class,'resumes_fines');       
+        return $this->belongsToMany(Fichier::class,'resumes_fines');
     }
     public function tableau_budgets() {
-        return $this->belongsToMany(Fichier::class,'tableau_budgets_fines');       
+        return $this->belongsToMany(Fichier::class,'tableau_budgets_fines');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -68,7 +68,7 @@ Route::post('forget_password', [AuthController::class, 'forget_password']);
 
 /*KPIss*/
  // Route avec filtres
-Route::get('/financements/kpi/par-domaine', [StatistiqueController::class, 'getKpiFinancementParDomaineFiltre']);
+Route::get('/financements/kpi/par-domaine', [StatistiqueController::class, 'getKpiFinancementParDomaine']);
 
 
  /**Statistique*/
